7f70ac35371d97038e7eee558e0da77b9dc8bce9
Zacharey Carey <zac4ncstate@aol.com>
2019-10-15 18:46:57 -0400

    pic fix

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,19 +2,18 @@ var friends = [
   {
     name: "Wanderlei Silva",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      "https://cdn.vox-cdn.com/thumbor/UaTlGEaJz1UR7D9FuzP-V_Vn0y0=/0x0:450x300/1200x800/filters:focal(0x0:450x300)/cdn.vox-cdn.com/imported_assets/929636/wanderlei-silva.jpg",
     scores: [4, 2, 2, 1, 4]
   },
   {
     name: "Cam Newton",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      "https://sportshub.cbsistatic.com/i/r/2019/09/17/79b0c87d-d117-4e24-89cd-0cedea451673/thumbnail/1200x675/496bf2e3c09410e21fcfb16f5b10ca4e/cam-newton-panthers.jpg",
     scores: [4, 3, 4, 4, 3]
   },
   {
     name: "Eva Green",
-    photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    photo: "https://upload.wikimedia.org/wikipedia/en/8/8e/Vesperlynd.jpeg",
     scores: [1, 1, 1, 2, 1]
   }
 ];
